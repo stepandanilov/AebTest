2a85551a85c09f3b3ed12e571b12858155d03ddf
stepandanilov <5digitnoob@gmail.com>
2021-07-01 12:53:35 +0900

    add new item page done

--- ToDoList/Controllers/ToDoController.cs
+++ ToDoList/Controllers/ToDoController.cs
@@ -16,6 +16,7 @@ namespace ToDoList.Controllers
         {
             this.context = context;
         }
+        //GET /
         public async Task<IActionResult> Index()
         {
             IQueryable<TodoListItem> items = from i in context.ToDoList orderby i.Id select i;
@@ -23,6 +24,23 @@ namespace ToDoList.Controllers
             List<TodoListItem> todoList = await items.ToListAsync();
 
             return View(todoList);
+        }
+        //GET /todo/create
+        public IActionResult Create() => View();
+        //POST /todo/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TodoListItem item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(item);
+
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return View(item);
         }
     }
 }
